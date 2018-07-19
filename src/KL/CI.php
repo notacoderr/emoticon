f42af91b1a5821b42cfc7b65a9bca3fdc397ad4c
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-07-19 10:34:41 +0800

    Update CI.php

--- src/KL/CI.php
+++ src/KL/CI.php
@@ -7,7 +7,6 @@ use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\{TextFormat, Config};
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
 
@@ -17,78 +16,63 @@ class CI extends PluginBase implements Listener
 {   
     
     public $formapi;
-	public $pipol;
 	
     public function onEnable()
     {
-
         $this->formapi = $this->getServer()->getPluginManager()->getPlugin('FormAPI');
-        $this->getLogger()->info("Beautifying....");
-		
-        $this->particles = new particlesUI($this);
-        $this->prefix = new prefixUI($this);
-        $this->size = new sizeUI($this);
-		$this->roles = new rolesUI($this);
-
-        $this->saveResource('main.yml');
-        $this->settings = new Config($this->getDataFolder() . "main.yml", CONFIG::YAML);
+        $this->getLogger()->info("Enabling...");
     }
 	
-    public function runCMD(string $c) : void
+    private function Announce(string $c) : void
     {
-        $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $c);
+        $this->getServer()->broadcast($c);
     }
 
     public function sendMainMenu(Player $player)
     {
         $form = $this->formapi->createSimpleForm(function (Player $player, array $data) {
-            if (isset($data[0])){
-                $button = $data[0];
-                switch ($button)
-                {
-					case 0:
-						$this->roles->mainForm($player);
-							break;
-                    case 1:	
-						$this->size->mainForm($player);
-							break;
-                    case 2: 
-						$this->prefix->mainForm($player);
-							break;
-					case 3:
-						$this->particles->mainForm($player);
-							break;
-                    default:
-							return;
-                }
-
-				return true;
-            }
+        if (isset($data[0])){
+		$button = $data[0];
+		$emoticon = $this->getEmotes()[$button];
+		$this->Announce("• " . TF::BOLD . TF::AQUA . $player->getDisplayName . TF::RESET . TF::BLACK . " >§c " . $emoticon);
         });
-        $form->setTitle('§l§fCosmetics');
-		$form->addButton('§l§0Roles', 1, $this->settings->get('roles-image')); //data[0]
-        $form->addButton('§l§0Size', 1, $this->settings->getNested('size.main')); //data[1]
-        $form->addButton('§l§0Custom Names', 1, $this->settings->get('prefix')); //data[2]
-		$form->addButton('§l§0Particles', 1, $this->settings->getNested('particles.main')); //data[3]
-        $form->addButton('§l§0Exit'); //data[4]
-        //$form->addButton('§l§eCapes', 1, $this->settings->getNested('store.image')); //data[3]
-
+		
+        $form->setTitle('§l§fEMOTES');
+	foreach($this->getEmotes() as $emote)
+	{
+		$form->addButton('§l§0' . $emote);
+	}
         $form->sendToPlayer($player);
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, String $label, array $args): bool
     {
-	  	if(!$sender instanceof Player){
-		  	$sender->sendMessage("Command must be run ingame!");
-		 	return true;
+	  	if(!$sender instanceof Player)
+		{
+			$sender->sendMessage("Command must be run ingame!");
+			return true;
 	  	}
 
-	  	switch(strtolower($cmd->getName())){
-            case "cui": case "servercosmetics": case "++":
+	  	switch( strtolower ( $cmd->getName() ))
+		{
+				
+            		case "ei": case "\":
 				$this->sendMainMenu($sender);
-            break;
-      }
-        return true;
-	}
+           		break;
+      		}
+	    return true;
+   }
+   
+   private function getEmotes() : array
+   {
+   	$emotes = array(
+			"( ͡° ͜ʖ ͡°)", "∠( ᐛ 」∠)＿", "(ﾟ⊿ﾟ)", "ᕕ( ᐛ )ᕗ", "_へ__(‾◡◝ )>",
+			"(/^▽^)/", "( ᐛ )و", "( ´ ▽ ` )ﾉ", "(´∇ﾉ｀*)ノ", "⁽ˇ́˙̫ˇ̀˵⁾", "(◡‿◡✿)", "✌(-‿-)✌",
+			"┌∩┐(ಠ_ಠ)┌∩┐", "(；¬д¬)", "(⁎˃ᆺ˂)", "( ｰ̀εｰ́ )", "(꒪⌓꒪)", "(ーー;)", "(*´﹃｀*)",
+			"(º﹃º )", "ɖී؀ීϸ", "＼(-_- )", "(￣^￣)ゞ", "ʕ•͡ɛ•͡ʼʼʔ", "(๑ↀᆺↀ๑)", "(.﹒︣︿﹒︣.)",
+			"(´-ι_-｀)"
+			);
+	return $emotes;
+   }
 
 }
